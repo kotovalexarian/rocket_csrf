3196c278c08fa8b86d424199b6725f088e5bbab3
Alex Kotov <kotovalexarian@gmail.com>
2020-10-18 12:17:37 +0500

    Test that authenticity token readed from cookies

---
+++ tests/guard.rs
@@ -0,0 +1,41 @@
+#![feature(decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use rand::RngCore;
+use rocket::http::Cookie;
+use rocket_csrf::CsrfToken;
+
+fn client() -> rocket::local::Client {
+    rocket::local::Client::new(rocket()).unwrap()
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .attach(rocket_csrf::Fairing::new())
+        .mount("/", routes![index])
+}
+
+#[get("/")]
+fn index(csrf_token: CsrfToken) -> String {
+    csrf_token.0
+}
+
+#[test]
+fn respond_with_valid_authenticity_token() {
+    let mut raw = [0u8; 32];
+    rand::thread_rng().fill_bytes(&mut raw);
+
+    let encoded = base64::encode(raw);
+
+    let body = client()
+        .get("/")
+        .private_cookie(Cookie::new("csrf_token", encoded.to_string()))
+        .dispatch()
+        .body()
+        .unwrap()
+        .into_string()
+        .unwrap();
+
+    assert_eq!(body, encoded);
+}
